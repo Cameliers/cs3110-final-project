f80651dd226d9a6308cdf2a997536dc406e8963e
Arman <am2738@cornell.edu>
2024-10-30 14:06:13 -0400

    Added new functions to main.ml
    - Added recursive prompt functions to prompt the match, team, and amount
    - Users must use a match from the list, a team from the match, and an amount less than or equal to their balance.

--- bin/main.ml
+++ bin/main.ml
@@ -38,13 +38,51 @@ let display_menu () =
 
 let matches_string =
   get_upcoming_matches ()
-  |> List.mapi (fun i (a, b) -> "(" ^ string_of_int i ^ ") " ^ a ^ " vs " ^ b)
+  |> List.mapi (fun i (a, b) ->
+         "(" ^ string_of_int (i + 1) ^ ") " ^ a ^ " vs " ^ b)
   |> String.concat "\n"
 
 let matches_list =
   List.map (fun (a, b) -> make_match a b "null") (get_upcoming_matches ())
 
 let user = make_user ()
+
+let rec prompt_number () =
+  let number = read_line () in
+  match number with
+  | "1" -> 0
+  | "2" -> 1
+  | "3" -> 2
+  | "4" -> 3
+  | "5" -> 4
+  | "6" -> 5
+  | "7" -> 6
+  | "8" -> 7
+  | "9" -> 8
+  | "10" -> 9
+  | _ ->
+      print_endline "Please choose a match from the list of 10.";
+      prompt_number ()
+
+let rec prompt_team (index : int) () =
+  let team = read_line () in
+  if
+    team = a_side (List.nth matches_list index)
+    || team = b_side (List.nth matches_list index)
+  then team
+  else (
+    print_endline "Please choose a team.";
+    prompt_team index ())
+
+let rec prompt_amount () =
+  let amount = read_line () in
+  if float_of_string amount <= balance user then float_of_string amount
+  else (
+    print_endline
+      ("Your balance is "
+      ^ string_of_float (balance user)
+      ^ ", please choose an amount less than or equal to your balance.");
+    prompt_amount ())
 
 (* [program_cycle] a Function that acts as the front/landing page of the
    program.*)
@@ -71,26 +109,28 @@ let rec program_cycle () =
             ^ " on " ^ bet_team bet)
           bet_list
       in
-      let final_string = String.concat "\n" bet_string_list in
+      let new_string = String.concat "\n" bet_string_list in
+      let final_string =
+        if bet_string_list = [] then "No bet history." else new_string
+      in
       print_endline ("Current Bet History:\n" ^ final_string);
       program_cycle ()
   | "4" ->
       print_endline "Choose a match from the matches.";
-      let user_input = read_line () in
-      let index = int_of_string user_input in
-      print_endline "Choose a team";
-      let team = read_line () in
-      print_endline "Choose an amount";
-      let amount = float_of_string (read_line ()) in
+      let index = prompt_number () in
+      print_endline "Choose a team.";
+      let team = prompt_team index () in
+      print_endline "Choose an amount.";
+      let amount = prompt_amount () in
       add_bet user (List.nth matches_list index) team amount;
-      print_endline "added bet!";
+      print_endline "Added bet!";
       program_cycle ()
   | "5" ->
-      display_title "Goodbye & Goodluck";
+      display_title "Goodbye & Good Luck!";
       exit 0
   | _ ->
       display_title
-        "Invalid Choice, please enter number *no paranthesis required*.";
+        "Invalid Choice, please enter number *no parenthesis required*.";
       program_cycle ()
 
 (* Entry point *)
