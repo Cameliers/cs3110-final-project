a76904f0d5314b4ec0fe1073f2cf4b11e7aca522
Arman <am2738@cornell.edu>
2024-12-10 15:47:25 -0500

    Added OUnit tests

--- lib/profile.ml
+++ lib/profile.ml
@@ -1,16 +1,15 @@
-
 (* Helper to convert a list of bets to a string *)
 let bets_to_string (bets : Bet.t list) : string =
-  bets 
-  |> List.map Bet.to_string  (* Assumes there is a [to_string] function in the Bet module *)
+  bets
+  |> List.map Bet.to_string
+     (* Assumes there is a [to_string] function in the Bet module *)
   |> String.concat ";"
 
 (* Helper to convert a string to a list of bets *)
 let string_to_bets (str : string) : Bet.t list =
   if str = "" then []
-  else str 
-        |> String.split_on_char ';' 
-        |> List.map Bet.of_string  (* Assumes there is an [of_string] function in the Bet module *)
+  else str |> String.split_on_char ';' |> List.map Bet.of_string
+(* Assumes there is an [of_string] function in the Bet module *)
 
 (* Save the user's profile to a file *)
 let save_to_file (filename : string) (user : User.t) : unit =
@@ -25,21 +24,29 @@ let save_to_file (filename : string) (user : User.t) : unit =
 (* Load the user's profile from a file *)
 let load_from_file (filename : string) : User.t =
   let ic = open_in filename in
-  
+
   (* Read each line of the file *)
   let balance = float_of_string (input_line ic) in
   let active_bets = string_to_bets (input_line ic) in
   let bet_history = string_to_bets (input_line ic) in
   close_in ic;
-  
+
   (* Create a new user and populate its fields *)
   let user = User.make_user () in
   User.change_balance user balance;
-  
+
   (* Add each active bet back to the user's profile *)
-  List.iter (fun bet -> User.add_bet user (Bet.match_of_bet bet) (Bet.team_of_bet bet) (Bet.amount_of_bet bet)) active_bets;
-  
-  (* Assuming the User module has a function to add a bet directly to the history, 
-      you may need a different approach if this is not allowed *)
-  List.iter (fun bet -> User.add_bet user (Bet.match_of_bet bet) (Bet.team_of_bet bet) (Bet.amount_of_bet bet)) bet_history;
+  List.iter
+    (fun bet ->
+      User.add_bet user (Bet.match_of_bet bet) (Bet.team_of_bet bet)
+        (Bet.amount_of_bet bet))
+    active_bets;
+
+  (* Assuming the User module has a function to add a bet directly to the
+     history, you may need a different approach if this is not allowed *)
+  List.iter
+    (fun bet ->
+      User.add_bet user (Bet.match_of_bet bet) (Bet.team_of_bet bet)
+        (Bet.amount_of_bet bet))
+    bet_history;
   user
--- test/test_final_project.ml
+++ test/test_final_project.ml
@@ -1,7 +1,76 @@
 open OUnit2
+open Final_project.Match
+open Final_project.User
+open Final_project.Bet
 
-let tests = "test suite" >::: [
-  "a trivial test" >:: (fun _ -> assert_equal 0 0)
-]
+let test_make_user _ =
+  let user = User.make_user () in
+  assert_equal 1000.0 user.User.balance;
+  assert_equal [] user.User.bets_active;
+  assert_equal [] user.User.bets_history
+
+let test_balance _ =
+  let user = User.make_user () in
+  assert_equal 1000.0 (User.balance user)
+
+let test_change_balance _ =
+  let user = User.make_user () in
+  User.change_balance user 500.0;
+  assert_equal 1500.0 user.User.balance;
+  User.change_balance user (-200.0);
+  assert_equal 1300.0 user.User.balance
+
+let test_add_bet _ =
+  let match1 = Match.make_match "TeamA" "TeamB" "2:1" in
+  let user = User.make_user () in
+  User.add_bet user match1 "TeamA" 100.0;
+  assert_equal 900.0 user.User.balance;
+  assert_equal 1 (List.length (User.bets_active user));
+  assert_equal 1 (List.length (User.bets_history user));
+  match User.bets_active user with
+  | [ { Bet.game; team; amount } ] ->
+      assert_equal "TeamA" team;
+      assert_equal 100.0 amount;
+      assert_equal "TeamA" (Match.a_side game);
+      assert_equal "TeamB" (Match.b_side game);
+      assert_equal "2:1" (Match.match_odds game)
+  | _ -> assert_failure "Expected one active bet"
+
+let test_bets_active_and_history _ =
+  let match1 = Match.make_match "TeamA" "TeamB" "2:1" in
+  let match2 = Match.make_match "TeamC" "TeamD" "3:2" in
+  let user = User.make_user () in
+  User.add_bet user match1 "TeamA" 100.0;
+  User.add_bet user match2 "TeamC" 200.0;
+  assert_equal 2 (List.length (User.bets_active user));
+  assert_equal 2 (List.length (User.bets_history user));
+  assert_equal 700.0 user.User.balance
+
+let test_match_creation _ =
+  let match1 = Match.make_match "TeamA" "TeamB" "2:1" in
+  assert_equal "TeamA" (Match.a_side match1);
+  assert_equal "TeamB" (Match.b_side match1);
+  assert_equal "2:1" (Match.match_odds match1)
+
+let test_bet_creation _ =
+  let match1 = Match.make_match "TeamA" "TeamB" "2:1" in
+  let bet = Bet.make_bet match1 "TeamA" 50.0 in
+  assert_equal 50.0 (Bet.bet_amount bet);
+  assert_equal "TeamA" (Bet.bet_team bet);
+  assert_equal "TeamA" (Match.a_side (Bet.bet_game bet));
+  assert_equal "TeamB" (Match.b_side (Bet.bet_game bet));
+  assert_equal "2:1" (Match.match_odds (Bet.bet_game bet))
+
+let tests =
+  "test_user_module"
+  >::: [
+         "test_make_user" >:: test_make_user;
+         "test_balance" >:: test_balance;
+         "test_change_balance" >:: test_change_balance;
+         "test_add_bet" >:: test_add_bet;
+         "test_bets_active_and_history" >:: test_bets_active_and_history;
+         "test_match_creation" >:: test_match_creation;
+         "test_bet_creation" >:: test_bet_creation;
+       ]
 
 let _ = run_test_tt_main tests
