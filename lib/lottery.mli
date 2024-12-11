c5ae18770e5d580ab2454628e8fddd5ff9d34d39
Saarang Bondalapati <sb2433@cornell.edu>
2024-12-10 23:56:58 -0500

    Added specification for lottery mli file

--- lib/lottery.mli
+++ lib/lottery.mli
@@ -1,1 +1,3 @@
 val spin_lottery : unit -> float
+(**[spin_lottery] returns a float value. The float value it returns is drawn from a normal distribution with mean 0 and standard deviation 10. 
+Values are restricted to be between -20 and 20.*)
