966169d3411a709949ea33549ea6ed50c3426e79
andrei <ac2923@cornell.edu>
2024-12-12 01:50:18 -0500

    fixed bets

--- lib/user.ml
+++ lib/user.ml
@@ -79,6 +79,9 @@ let complete_bets t =
       let id = match_id bet_match in
       let result = get_match_result id in
       match result with
+      | "Error parsing match result" -> ()
+      | "Unexpected error" -> ()
+      | "Error fetching match result" -> ()
       | "Draw" ->
           t.bets_active <-
             List.filter
